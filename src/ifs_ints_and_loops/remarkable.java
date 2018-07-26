dac666348b0c8c124d5fd34ed69b50403fff0b7f
komodochristopher <league@WTS7.attlocal.net>
2018-07-26 14:59:22 -0700

    un birthday and remarkable

---
+++ src/ifs_ints_and_loops/remarkable.java
@@ -0,0 +1,12 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+public class remarkable {
+public static void main(String[] args) {
+String name=JOptionPane.showInputDialog("what is your name human");
+if(name.equalsIgnoreCase("brandon"))
+
+
+
+}
+}
---
+++ src/ifs_ints_and_loops/unbirthday.java
@@ -0,0 +1,17 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+	public static void main(String[] args) {
+		String birthday=JOptionPane.showInputDialog("when is your birthday");
+if(birthday.equals ("7/26"))
+		{
+JOptionPane.showMessageDialog(null,"happy birthday");
+		}
+
+else{JOptionPane.showMessageDialog(null,"happy unbirthday");}
+	}
+
+
+}
