f6fad18d333dc73d5532575bbb77079d0b8011f4
league <league@wts9.attlocal.net>
2018-08-25 17:27:05 -0700

    remarkable

--- src/ifs_ints_and_loops/remarkable.java
+++ src/ifs_ints_and_loops/remarkable.java
@@ -1,12 +1,17 @@
 package ifs_ints_and_loops;
 
 import javax.swing.JOptionPane;
+
 public class remarkable {
-public static void main(String[] args) {
-String name=JOptionPane.showInputDialog("what is your name human");
-if(name.equalsIgnoreCase("brandon"))
-
-
-
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+		String name = JOptionPane.showInputDialog("what is your name human");
+		if (name.equalsIgnoreCase("brandon")) {
+			JOptionPane.showMessageDialog(null, "you are smart");
+			if (name.equalsIgnoreCase("nathan")) {
+				JOptionPane.showMessageDialog(null, "you like fortnite");
+			}
+		}
+	}
 }
 }
