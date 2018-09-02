ae6bd80e735dd4825ac1220dc107aa858ddba240
komodochristopher <league@WorthyMini-5.attlocal.net>
2018-09-01 17:29:57 -0700

    remarkable

--- src/ifs_ints_and_loops/remarkable.java
+++ src/ifs_ints_and_loops/remarkable.java
@@ -4,14 +4,21 @@ import javax.swing.JOptionPane;
 
 public class remarkable {
 	public static void main(String[] args) {
-		for(int i=0;i<10;i++) {
-		String name = JOptionPane.showInputDialog("what is your name human");
-		if (name.equalsIgnoreCase("brandon")) {
-			JOptionPane.showMessageDialog(null, "you are smart");
+		for (int i = 0; i < 10; i++) {
+			String name = JOptionPane.showInputDialog("what is your name human");
+			if (name.equalsIgnoreCase("brandon")) {
+				JOptionPane.showMessageDialog(null, "you are smart");
+			}
 			if (name.equalsIgnoreCase("nathan")) {
 				JOptionPane.showMessageDialog(null, "you like fortnite");
 			}
+				if(name.equalsIgnoreCase("adam")) {
+		        JOptionPane.showMessageDialog(null,"adamberg vs rainbowlishes land adamberg says make adamberg win  blah blah blah!");
+			}
+				if(name.equalsIgnoreCase("samuel")) {
+		        JOptionPane.showMessageDialog(null,"minecraft is awesome- mine diamonds........is that all you like.........- -_    (^_^)");
+		 
 		}
-	}
+		}
 }
 }
