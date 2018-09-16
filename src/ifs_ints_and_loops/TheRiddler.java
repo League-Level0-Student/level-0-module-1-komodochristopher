6c2cd739d87092244ae0a9b555b6bf40f3749b63
aryanmshah <league@WTS22.attlocal.net>
2018-09-15 17:28:56 -0700

    the riddler

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -1,17 +1,18 @@
 package ifs_ints_and_loops;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
+
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+    String score= 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+JOptionPane.showInputDialog(null, "what person is the leader of _________  army and has fought against rainbowlishes land?")
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
 		// 5. Otherwise, say "wrong" and tell them the answer
@@ -22,4 +23,3 @@ public class TheRiddler {
 		
 	}
 }
-
