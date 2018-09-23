ddaac391d401cdfd30f15e00d3391c24eb04e9d3
komodochristopher <league@WTS22.attlocal.net>
2018-09-22 17:26:33 -0700

    the riddler

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -10,16 +10,19 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-    String score= 0;
+		String score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-JOptionPane.showInputDialog(null, "what person is the leader of _________  army and has fought against rainbowlishes land?")
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		JOptionPane.showInputDialog(null,
+				"what person is the leader of _________  army and has fought against rainbowlishes land?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		String answer = JOptionPane.showMessageDialog(null, "adam");
 
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
+
 	}
 }
