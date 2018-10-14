1203fbf0cae887faba302f093f7a252b5d2a5071
Dong Zou <jiwenzou@gmail.com>
2018-10-13 17:27:03 -0700

    badgers lyrics

---
+++ src/ifs_ints_and_loops/badgerslyrics.java
@@ -0,0 +1,18 @@
+package ifs_ints_and_loops;
+
+public class badgerslyrics {
+public static void main(String[] args) {
+for(int i=0;i<11;i++) {
+System.out.println("badger");
+for(int e=0;e<2;e++) {
+System.out.println("mushroom");
+for(int h=0;h<5;h++)
+System.out.println("snakes");
+
+}
+}
+}
+}
+
+
+
